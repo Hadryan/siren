0a97709a75d5f9cd85c223c9cbbaa0a4efc0348a
moonou <moonoulong@gmail.com>
2018-03-06 13:55:11 +0800

    init

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, Image, Text, findNodeHandle, StyleSheet } from 'react-native';
+import { BlurView } from 'react-native-blur';
+
+const uri = 'https://odujw1uur.qnssl.com/584_20150201101431663.jpg';
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { viewRef: null };
+  }
+
+  imageLoaded() {
+    this.setState({ viewRef: findNodeHandle(this.backgroundImage) });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          ref={(img) => { this.backgroundImage = img; }}
+          source={{uri}}
+          style={styles.absolute}
+          onLoadEnd={this.imageLoaded.bind(this)}
+        />
+        <BlurView
+          style={styles.absolute}
+          viewRef={this.state.viewRef}
+          blurType="light"
+          blurAmount={10}
+        />
+        <Text>Hi, I am some unblurred text</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  absolute: {
+    position: "absolute",
+    top: 0, left: 0, bottom: 0, right: 0,
+  },
+});
