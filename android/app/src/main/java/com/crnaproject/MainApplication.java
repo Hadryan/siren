122b02d364d185aa9c8843503884c1931a9c5b1b
moonou <moonoulong@gmail.com>
2018-03-29 11:09:40 +0800

    add LinearGradient component

--- android/app/src/main/java/com/crnaproject/MainApplication.java
+++ android/app/src/main/java/com/crnaproject/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.BV.LinearGradient.LinearGradientPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,8 @@ public class MainApplication extends NavigationApplication {
           new FastImageViewPackage(),
           new TrackPlayer(),
           new BlurViewPackage(),
-          new AesCryptoPackage()
+          new AesCryptoPackage(),
+          new LinearGradientPackage()
     );
   }
 
